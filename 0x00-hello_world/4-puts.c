5392b6c3ea428e0f9e87ee2e6ddb22a9e5616e19
Vincent-Musyimi <vincentmusyimi40@gmail.com>
2022-09-08 13:24:05 -0700

    Using puts instead of printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdioh.h>
+
+int main(void)
+{
+	/* We are printing out using the puts command
+	 * no printf -*/
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
