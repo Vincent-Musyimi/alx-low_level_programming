c5ce9e7a7158881d5782b420198a0c7f16c6c6de
Vincent-Musyimi <vincentmusyimi40@gmail.com>
2022-09-08 14:46:45 -0700

    the print function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /*
- * main - Entry point
+ * main - Main function
  * Description: using the puts function
  * prints 'program prints programming languages being multilingual'
  * return: 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);	
 }
