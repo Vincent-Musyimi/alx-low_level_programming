df766e82f85fa5ed2ad70c83f27def96a29d0fe8
Vincent-Musyimi <vincentmusyimi40@gmail.com>
2022-09-08 14:08:22 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/* Using puts
  * main - String of our function
  *
  * Description: program prints programming languages being multilingual
@@ -10,5 +10,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);	
+	return (0);	
 }
