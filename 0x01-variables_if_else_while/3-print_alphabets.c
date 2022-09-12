dd45fa4741e9caff2b9e10a28a72f492b4fa670b
Vincent-Musyimi <vincentmusyimi40@gmail.com>
2022-09-12 13:35:01 -0700

    Alphabets in both caps

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Alphabets in both caps
+ * this program prints alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	char ai;
+
+	for (ai = 'a' ; ai <= 'z' ; ai++)
+	{
+		putchar(ai);
+	}
+	for (ai = 'A' ; ai <= 'Z' ; ai++)
+	{
+		putchar(ai);
+	}
+	putchar('\n');
+	return 0;
+}
