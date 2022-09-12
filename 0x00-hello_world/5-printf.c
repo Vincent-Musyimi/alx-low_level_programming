7a1774aeec89e2140cfbf80ad78740c35d934446
Vincent-Musyimi <vincentmusyimi40@gmail.com>
2022-09-12 11:15:46 -0700

    Print F

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,7 @@
  * Description: using the main function
  * Return: 0
  */
-int main(void)
-{
+int main(void) {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,15 @@
  * this program prints the prints on comp sizes
  * return: 0
  */
-int main(void)
-{
+int main(void) {
 	char c;
 	int i;
 	long li;
 	long long lli;
 	float f;
 
-	printf("Print size of char: %ld byte(s)\n", sizeof(c));
-	printf("print size of int: %ld byte(s)\n", sizeof(i));
+	printf("Print size of char: %d byte(s)\n", sizeof(c));
+	printf("print size of int: %d byte(s)\n", sizeof(i));
 	printf("Print size of long: %d byte(s)\n", sizeof(li));
 	printf("Print size of long long: %d byte(s)\n", sizeof(lli));
 	printf("Print size of float: %d byte(s)\n", sizeof(f));
