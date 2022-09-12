3aa01e754580547202eca6d98ab6e76d3a84e50f
Vincent-Musyimi <vincentmusyimi40@gmail.com>
2022-09-12 13:17:49 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Alphabets
+ * this program prints "Alphabets in small caps"
+ * Return: 0
+ */
+int main(void)
+{
+	char al;
+	for(al = 'a' ;  al <= 'z' ; al++)
+	{
+		putchar(al);
+	}
+	putchar('\n');
+	return (0);
+}
