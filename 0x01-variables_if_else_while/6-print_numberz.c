5c8ef65a14da35e50632f8c1e93e4a48be5888bf
Vincent-Musyimi <vincentmusyimi40@gmail.com>
2022-09-14 14:01:34 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,11 +9,11 @@
  */
 int main(void)
 {
-	int i;
+	int i = 0;
 
-	for (i = '0' ; i < '10' ; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
-		putchar(i);
+		putchar('0' + i);
 	}
 	putchar('\n');
 	return (0);
