7d6798b3f21a7391a27fd4dc02477521c8d70d2e
Vincent-Musyimi <vincentmusyimi40@gmail.com>
2022-09-14 12:49:46 -0700

    Printig numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: all single digits of base 10
+ * Program prints "all single digits"
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		printf ("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
+
+ 
