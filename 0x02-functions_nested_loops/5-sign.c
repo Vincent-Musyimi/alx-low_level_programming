0597d03cee1290800ddf4cbca202ec9d38effda1
Vincent-Musyimi <vincentmusyimi40@gmail.com>
2022-09-16 07:56:29 -0700

    Checking greater than )

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - function to 
+ *
+ * Int n: argument to check the function
+ * @n: argument to check the function
+ * Return: 0;
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+		_putchar('0')
+			return (0);
+}
