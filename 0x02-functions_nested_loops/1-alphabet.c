4dc57f9dd782437b8d01521cbd4f2e7575397466
Vincent-Musyimi <vincentmusyimi40@gmail.com>
2022-09-16 06:40:59 -0700

    Printing alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: print alphabet
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
