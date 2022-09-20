0839cbdc820da559f4198f544d99006957ec518c
Vincent-Musyimi <vincentmusyimi40@gmail.com>
2022-09-20 12:18:28 -0700

    Tasks - 98 Battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
  * reset_to_98 - entry point
  *
  * int *n - argument for the function
- * *n - argumneent for the function
+ * @*n - argumneent for the function
  * Return: 0
  */
 void reset_to_98(int *n)
