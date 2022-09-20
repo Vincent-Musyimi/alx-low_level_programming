d6a0a37b2db070a3445667b0c09227d0f913de46
Vincent-Musyimi <vincentmusyimi40@gmail.com>
2022-09-20 12:07:42 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <main.h>
+#include <stdio.h>
+/**
+ * reset_to_98 - entry point
+ *
+ * int *n - argument for the function
+ * *n - argumneent for the function
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
