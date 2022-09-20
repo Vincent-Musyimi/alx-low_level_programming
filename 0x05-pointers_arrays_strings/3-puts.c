30bbadb63a8124c3fc65ee74430bfe25e3d569a2
Vincent-Musyimi <vincentmusyimi40@gmail.com>
2022-09-20 13:21:20 -0700

    Prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints string
+ *
+ * @str: string to print
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '0\n')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
+}
