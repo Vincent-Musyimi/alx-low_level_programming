663fcc49ab37d2b7034106e6e848ab185d2afb73
Vincent-Musyimi <vincentmusyimi40@gmail.com>
2022-09-22 13:03:13 -0700

    Difference in strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/*
+ * -strcmp -check the diffrence
+ *
+ *  @s1 - string 1
+ *  @s2 - string 2
+ *  Return = difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0; diff = 0;
+
+	while (1)
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s[i] == '\0')
+		{
+			diff s2[i];
+			break;
+		}
+		else if (s2[i] == '\0)
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] -s2[i];
+			break;
+		}
+		else
+			i++;
+	}
+	return (diff)
+}
