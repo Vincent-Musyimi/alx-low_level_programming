0e44f5483de4d7323ba60c7d82b22fc0fad4707c
Vincent-Musyimi <vincentmusyimi40@gmail.com>
2022-09-22 12:43:29 -0700

    copies from source

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+ * _strncpy - function that copys string
+ *
+ * @dest: buffer stroring string
+ * @src: source of string
+ * @n: limit
+ * Return = dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i < 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
